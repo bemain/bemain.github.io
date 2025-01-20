ea2865318d1da792c09c205988e500f263d57e01
bemain <bemain.dev@gmail.com>
2025-01-20 20:52:24 +0100

    Add more moments

--- lib/writing/article_pane.dart
+++ lib/writing/article_pane.dart
@@ -27,12 +27,31 @@ final List<Article> articles = [
     writtenAt: DateTime(2025, 1, 20),
   ),
   Article(
-    id: "250120",
-    title: "Highland green",
+    id: "230712",
+    title: "Mountain-fog and highland-green",
     textPath: "assets/writing/250120.md",
-    image: AssetImage("assets/writing/250120.jpg"),
-    writtenAt: DateTime(2023, 4, 15),
-  )
+    image: AssetImage("assets/writing/230712.jpg"),
+    writtenAt: DateTime(2023, 7, 12),
+  ),
+  Article(
+    id: "21000",
+    title: "What is already dead can never die",
+    textPath: "assets/writing/21000.md",
+    writtenAt: DateTime(2021, 7, 24),
+  ),
+  Article(
+    id: "21001",
+    title: "Jag tänker aldrig bli psykolog",
+    textPath: "assets/writing/21001.md",
+    writtenAt: DateTime(2021, 11, 17),
+  ),
+  Article(
+    id: "220421",
+    title: "När kvällen åter står i brand",
+    textPath: "assets/writing/220421.md",
+    image: AssetImage("assets/writing/220421.jpg"),
+    writtenAt: DateTime(2022, 4, 21),
+  ),
 ];
 
 class ArticlePane extends StatelessWidget {
