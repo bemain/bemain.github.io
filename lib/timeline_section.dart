6c2fb9b289c9a1416537ef6b834800823719a1bb
bemain <bemain.dev@gmail.com>
2025-01-30 16:37:31 +0100

    Add event types

--- lib/timeline_section.dart
+++ lib/timeline_section.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio/layout.dart';
 
+enum EventType {
+  education,
+  work,
+  coding,
+  music,
+  award,
+  other,
+}
+
 class Event {
   // TODO: Add links to events
-  // TODO: Add event types
   Event({
     required this.title,
+    required this.type,
     this.summary,
     this.description,
     this.location,
@@ -14,6 +23,8 @@ class Event {
 
   /// The title of the event.
   final String title;
+
+  final EventType type;
 
   /// Where the event took place.
   /// TODO: Show this somewhere? Make clickable?
@@ -36,6 +47,7 @@ class TimelineSection extends StatelessWidget {
     Event(
       dateString: "2024 - present",
       title: "Chalmers University of Technology",
+      type: EventType.education,
       location: "Gothenburg, Sweden",
       summary: "Engineering Mathematics",
       description: """Began studies in Engineering Mathematics.""",
@@ -43,6 +55,7 @@ class TimelineSection extends StatelessWidget {
     Event(
       dateString: "Autumn 2023",
       title: "EC Solutions",
+      type: EventType.work,
       location: "Helsingborg, Sweden",
       summary: "Consulting firm",
       description:
@@ -51,14 +64,32 @@ class TimelineSection extends StatelessWidget {
     Event(
       dateString: "Spring 2023",
       title: "Conductor in the Broadway musical 'Annie'",
+      type: EventType.music,
       location: "Helsingborgs Stadsteater, Lunds Stadsteater",
       summary: "Conducted the band and arranged the music.",
       description:
           """The musical Annie was staged in 2023 at the city theaters in Lund and Helsingborg, and I conducted the band of 12 people and arranged much of the music. I also appeared on stage as one of the actors.""",
     ),
     Event(
+      dateString: "2022",
+      title: "Award, Anderssons minnesfond",
+      type: EventType.award,
+      summary: "Musical award given to talented youths.",
+      description:
+          "Musical award given to talented youths in the municipality of Helsingborg, to be used for continued studies in classical music",
+    ),
+    Event(
+      dateString: "2021",
+      title: "Award, Lund diocese",
+      type: EventType.award,
+      summary: "Award for musically engaged youths",
+      description:
+          "Yearly award given to musically engaged youths in the Lund diocese.",
+    ),
+    Event(
       dateString: "2020 - 2022",
       title: "Coding instructor at Hemkodat",
+      type: EventType.work,
       location: "Lund, Sweden",
       summary: "Taught children to code.",
       description:
@@ -67,6 +98,7 @@ class TimelineSection extends StatelessWidget {
     Event(
       dateString: "2020 - 2023",
       title: "Lars-Erik Larsson-gymnasiet",
+      type: EventType.education,
       location: "Lund, Sweden",
       summary: "Natural Science, Music",
       description: """Studied Natural Science, Music. 
@@ -75,6 +107,7 @@ Grade: 22,41""",
     Event(
       dateString: "Summer 2017",
       title: "Mattekollo",
+      type: EventType.other,
       location: "Karlskrona, Sweden",
       summary: "Summer camp for mathematically gifted children",
       description:
@@ -86,56 +119,50 @@ Grade: 22,41""",
   Widget build(BuildContext context) {
     final WindowSize windowSize = WindowSize.of(context);
 
-    switch (windowSize) {
-      case WindowSize.compact:
-      case WindowSize.medium:
-        return Padding(
-          padding: windowSize.margin.add(EdgeInsets.symmetric(vertical: 32)),
-          child: Column(
+    return Padding(
+      padding: windowSize.margin.add(EdgeInsets.symmetric(vertical: 32)),
+      child: Column(
+        children: [
+          const SizedBox(height: 24),
+          Row(
             children: [
-              const SizedBox(height: 24),
-              Row(
-                children: [
-                  CircleAvatar(
-                    backgroundColor:
-                        Theme.of(context).colorScheme.surfaceContainerLow,
-                    radius: 24,
-                    child: Icon(Icons.event_outlined),
-                  ),
-                  const SizedBox(width: 16),
-                  _buildTitle(context),
-                ],
+              CircleAvatar(
+                backgroundColor:
+                    Theme.of(context).colorScheme.surfaceContainerLow,
+                radius: 24,
+                child: Icon(Icons.event_outlined),
               ),
-              const SizedBox(height: 8),
-              Stack(
-                children: [
-                  Positioned.fill(
-                    child: Align(
-                      alignment: Alignment.topLeft,
-                      child: SizedBox(
-                        width: 48,
-                        child: FractionallySizedBox(
-                          heightFactor: 1 - 1 / (events.length * 2),
-                          child: VerticalDivider(),
-                        ),
-                      ),
-                    ),
-                  ),
-                  IntrinsicHeight(
-                    child: Column(
-                      children: [
-                        for (final event in events) EventTile(event: event),
-                      ],
-                    ),
-                  )
-                ],
-              ),
+              const SizedBox(width: 16),
+              _buildTitle(context),
             ],
           ),
-        );
-      default:
-        return Placeholder();
-    }
+          const SizedBox(height: 8),
+          Stack(
+            children: [
+              Positioned.fill(
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: SizedBox(
+                    width: 48,
+                    child: FractionallySizedBox(
+                      heightFactor: 1 - 1 / (events.length * 2),
+                      child: VerticalDivider(),
+                    ),
+                  ),
+                ),
+              ),
+              IntrinsicHeight(
+                child: Column(
+                  children: [
+                    for (final event in events) EventTile(event: event),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
   }
 
   Widget _buildTitle(
@@ -167,7 +194,7 @@ class EventTile extends StatelessWidget {
             color: Theme.of(context).colorScheme.surface,
           ),
           child: Icon(
-            Icons.radio_button_checked_outlined,
+            _getIcon(),
             color:
                 Theme.of(context).colorScheme.onSurfaceVariant.withAlpha(0xaa),
           ),
@@ -181,6 +208,23 @@ class EventTile extends StatelessWidget {
         ),
       ],
     );
+  }
+
+  IconData _getIcon() {
+    switch (event.type) {
+      case EventType.education:
+        return Icons.school_outlined;
+      case EventType.work:
+        return Icons.work_outline;
+      case EventType.coding:
+        return Icons.code_outlined;
+      case EventType.music:
+        return Icons.music_note_outlined;
+      case EventType.award:
+        return Icons.emoji_events_outlined;
+      case EventType.other:
+        return Icons.radio_button_checked_outlined;
+    }
   }
 
   Widget _buildText(BuildContext context) {
